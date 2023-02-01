0332c6f661cdf7966be7bfdcace6d1157211c33d
AhmedMaiez-am <ahmeddhiaeddine.maiez@esprit.tn>
2023-02-01 13:22:40 +0100

    update

--- src/app.jsx
+++ src/app.jsx
@@ -8,10 +8,69 @@ import Timeline from "./components/timeline/timeline";
 import Footer from "./components/footer/footer";
 
 function WebContent() {
+  const Editorial = () => (
+    <svg className="editorial"
+         xmlns="http://www.w3.org/2000/svg"
+         xmlnsXlink="http://www.w3.org/1999/xlink"
+         viewBox="0 24 150 28"
+         preserveAspectRatio="none">
+      <defs>
+        <path id="gentle-wave"
+              d="M-160 44c30 0 
+                 58-18 88-18s
+                 58 18 88 18 
+                 58-18 88-18 
+                 58 18 88 18
+                 v44h-352z" />
+      </defs>
+      <g className="parallax1">
+        <use xlinkHref="#gentle-wave" x="50" y="3" fill="url(#gradient1)"/>
+      </g>
+      <g className="parallax2">
+        <use xlinkHref="#gentle-wave" x="50" y="0" fill="url(#gradient2)"/>
+      </g>
+      <g className="parallax3">
+        <use xlinkHref="#gentle-wave" x="50" y="9" fill="url(#gradient3)"/>
+      </g>
+      <g className="parallax4">
+        <use xlinkHref="#gentle-wave" x="50" y="6" fill="url(#gradient4)"/>  
+      </g>
+      <linearGradient id="gradient1" x1="0" y1="0" x2="0" y2="1">
+        <stop offset="0%" stopColor="rgba(244, 97, 193, 0)" />
+        <stop offset="100%" stopColor="#f461c1" />
+      </linearGradient>
+      <linearGradient id="gradient2" x1="0" y1="0" x2="0" y2="1">
+        <stop offset="0%" stopColor="rgba(69, 121, 226, 0)" />
+        <stop offset="100%" stopColor="#4579e2" />
+      </linearGradient>
+      <linearGradient id="gradient3" x1="0" y1="0" x2="0" y2="1">
+        <stop offset="0%" stopColor="rgba(52, 97, 193, 0)" />
+        <stop offset="100%" stopColor="#3461c1" />
+      </linearGradient>
+      <linearGradient id="gradient4" x1="0" y1="0" x2="0" y2="1">
+        <stop offset="0%" stopColor="rgba(255, 255, 255, 0)" />
+        <stop offset="100%" stopColor="#fff" />
+      </linearGradient>
+    </svg>
+  );
   return (
     <>
+    <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+            
       <div className="bg-circle1"></div>
       <div className="bg-circle2"></div>
+      
       <Header />
       <Navbar />
       <Home />
@@ -20,6 +79,7 @@ function WebContent() {
       <Timeline />
       <Contact />
       <Footer />
+      <Editorial/> 
     </>
   );
 }
--- src/components/qualifications/qualifications.jsx
+++ src/components/qualifications/qualifications.jsx
@@ -4,74 +4,72 @@ const Qualifications = () => {
   return (
     <div>
       <main>
-<h2>
-        <span>My Qualifications</span> <br />
-
-      </h2>
-	  </main>
+        <h2>
+          <span>My Qualifications</span> <br />
+        </h2>
+      </main>
       <ul class="tilesWrap">
-	<li>
-		<h2>React</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-	<li>
-		<h2>HTML</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-	<li>
-		<h2>CSS</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-	<li>
-		<h2>JavaScript</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>Java</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>SpringBoot</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>Angular</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>Laravel</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>Symfony</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>TypeScript</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>Python</h2>
-		<button>★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>SQL</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>MongoDB</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>DevOps</h2>
-		<button>★ ★ ★ ★</button>
-	</li>
-  <li>
-		<h2>Office</h2>
-		<button>★ ★ ★ ★ ★</button>
-	</li>
-  
-</ul>
+        <li>
+          <h2>React</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>HTML</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>CSS</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>JavaScript</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>Java</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>SpringBoot</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>Angular</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>Laravel</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>Symfony</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>TypeScript</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>Python</h2>
+          <button>★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>SQL</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>MongoDB</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>DevOps</h2>
+          <button>★ ★ ★ ★</button>
+        </li>
+        <li>
+          <h2>Office</h2>
+          <button>★ ★ ★ ★ ★</button>
+        </li>
+      </ul>
     </div>
   );
 };
